7ec406ef000baaebdc9dc55e1aee64e391cadd52
U-HBO\foliveir <foliveir@NYXFOLIVEIR1Z20.hbo.homebox.com>
2011-03-09 14:59:00 -0500

    changing the name to geo i didnt realize theres a geonode already on npm

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# GeoNode for Node.js
+# Geo for Node.js
 by Felipe Oliveira (http://twitter.com/_felipera)
 
-GeoNode is a basic, but simple, geocode library for Node.js using Google's Geocode API (V3) - Geo Spatial capability hopefully is coming out soon.
+Geo is a very basic, but simple, geocode library for Node.js using Google's Geocode API (V3) - Geo Spatial features are coming out soon.
 
 ## Usage
 
-		var geonode = require('../lib/geonode');
+		var geo = require('../lib/geo');
 		
 		var address = '885 6th Ave #15D New York, NY 10001';
 		var sensor = false;
-		geonode.geocoder(geonode.google, address, sensor, function(formattedAddress, latitude, longitude) {
+		geo.geocoder(geo.google, address, sensor, function(formattedAddress, latitude, longitude) {
 			console.log("Formatted Address: " + formattedAddress);
 			console.log("Latitude: " + latitude);
 			console.log("Longitude: " + longitude);
--- examples/geocode_lookup.js
+++ examples/geocode_lookup.js
@@ -1,18 +1,18 @@
 // *********************************************
 // *********************************************
-// GeoNode by Felipe Oliveira
+// Geo by Felipe Oliveira
 // March 2011
-// http://geeks.aretotally.in/projects/geonode
+// http://geeks.aretotally.in/projects/geo
 // http://twitter.com/_felipera
 // *********************************************
 // *********************************************
 
-var geonode = require('../lib/geonode');
+var geo = require('../lib/geo');
 
 // Good Address
 var address = '885 6th Ave #15D New York, NY 10001';
 var sensor = false;
-geonode.geocoder(geonode.google, address, sensor, function(formattedAddress, latitude, longitude) {
+geo.geocoder(geo.google, address, sensor, function(formattedAddress, latitude, longitude) {
 	console.log("Formatted Address: " + formattedAddress);
 	console.log("Latitude: " + latitude);
 	console.log("Longitude: " + longitude);
@@ -21,7 +21,7 @@ geonode.geocoder(geonode.google, address, sensor, function(formattedAddress, lat
 // Bad Address
 var address = '1111 Foobar Ave #15D New York, NY 9999';
 var sensor = false;
-geonode.geocoder(geonode.google, address, sensor, function(formattedAddress, latitude, longitude) {
+geo.geocoder(geo.google, address, sensor, function(formattedAddress, latitude, longitude) {
 	console.log("Formatted Address: " + formattedAddress);
 	console.log("Latitude: " + latitude);
 	console.log("Longitude: " + longitude);
